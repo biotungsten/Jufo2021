89513d652931b4ca5e188dc57df26c1b2b9be138
David Sauer <sauerdavid13@icloud.com>
2020-12-20 13:16:59 +0100

    added

--- experiments/derivatives/plot.py
+++ experiments/derivatives/plot.py
@@ -1,6 +1,7 @@
 import csv
 import numpy as np
 import matplotlib.pyplot as plt
+from scipy import stats
 data = {}
 
 with open("aggregated_data.csv", "r") as file:
@@ -18,6 +19,13 @@ ax = plt.subplot(111)
 r1 = np.arange(len(data.keys()))
 r2 = [x + barWidth for x in r1]
 
+ctrl=data["CTRL"]
+for key, value in data.items():
+    p2 = stats.ttest_ind_from_stats(value[0], value[2], 4, ctrl[0], ctrl[2], 4, equal_var=False).pvalue
+    p7 = stats.ttest_ind_from_stats(value[1], value[3], 4, ctrl[1], ctrl[3], 4, equal_var=False).pvalue
+    t2 = stats.ttest_ind_from_stats(value[0], value[2], 4, ctrl[0], ctrl[2], 4, equal_var=False).statistic
+    t7 = stats.ttest_ind_from_stats(value[1], value[3], 4, ctrl[1], ctrl[3], 4, equal_var=False).statistic
+    print(f"{key} p2:{round(p2/2,4)}, p7:{round(p7/2,4)}; t2:{round(t2,4)}, t7:{round(t7,4)}") #divide by two because one tailed
 def stripNamesOfNumber(str):
     new_str = ""
     for n in str:
@@ -28,8 +36,15 @@ def stripNamesOfNumber(str):
     return new_str
 
 ax.bar(r1, mean2, width = barWidth, color = 'orange', edgecolor = 'black', yerr=[sd2, sd2], capsize=7, label='1 Tag', error_kw=dict(capsize=2, elinewidth=0.5))
-ax.bar(r2, mean7, width = barWidth, color = 'red', edgecolor = 'black', yerr=[sd7, sd7], capsize=7, label='7 Tage', error_kw=dict(capsize=2, elinewidth=0.5))
+ax.bar(r2, mean7, width = barWidth, color = 'red', edgecolor = 'black', yerr=[sd7, sd7], capsize=7, label='6 Tage', error_kw=dict(capsize=2, elinewidth=0.5))
 ax.set_xticks([r + barWidth for r in range(len(data.keys()))])
+ax.text(x=r1[0]-0.08,y=0.07,s="**",rotation=180, size=5)
+ax.text(x=r2[0]-0.08,y=0.23,s="**",rotation=180, size=5)
+ax.text(x=r1[1]-0.08,y=0.01,s="**",rotation=180, size=5)
+ax.text(x=r2[1]-0.08,y=0.25,s="**",rotation=180, size=5)
+ax.text(x=r1[2]-0.08,y=0.1,s="**",rotation=180, size=5)
+ax.text(x=r2[2]-0.08,y=0.41,s="**",rotation=180, size=5)
+ax.text(x=r2[3]-0.05,y=0.72,s="*",rotation=180, size=5)
 ax.set_xticklabels([stripNamesOfNumber(n) for n in data.keys()], rotation=45)
 ax.set_ylabel("Keimungsrate [%]")
 ax.legend(frameon=False)
